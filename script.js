fcf352ff50a518ad3ff4754957079a3767dcdec2
Levani49 <levani.pachuashvili.1@btu.edu.ge>
2023-07-20 00:03:20 +0400

    all buttons work

--- script.js
+++ script.js
@@ -1,29 +1,99 @@
-function displayClock() {
-  var now = new Date();
+var cubes = [];
 
-  // Format and display the date
-  var dateElement = document.getElementById("date");
-  var date = now.getDate();
-  var month = now.getMonth() + 1;
-  var year = now.getFullYear();
-  dateElement.textContent =
-    formatDate(date) + "/" + formatDate(month) + "/" + year;
-
-  // Format and display the time
-  var clockElement = document.getElementById("clock");
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
-  clockElement.textContent =
-    formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+// Generate a random cube object
+function generateCube() {
+  var cube = {
+    id: cubes.length + 1,
+    size: getRandomSize(),
+    color: getRandomColor(),
+  };
+  return cube;
 }
 
-function formatTime(time) {
-  return time < 10 ? "0" + time : time;
+// Generate a random size for the cube
+function getRandomSize() {
+  return Math.floor(Math.random() * 100) + 1;
 }
 
-function formatDate(date) {
-  return date < 10 ? "0" + date : date;
+// Generate a random color for the cube
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
-setInterval(displayClock, 1000);
+function renderCubes() {
+  var cubeContainer = document.getElementById("cube-container");
+  cubeContainer.innerHTML = "";
+
+  cubes.forEach(function (cube) {
+    var cubeElement = document.createElement("div");
+    cubeElement.className = "cube";
+    cubeElement.style.width = cube.size + "px";
+    cubeElement.style.height = cube.size + "px";
+    cubeElement.style.backgroundColor = cube.color;
+    cubeElement.innerHTML = cube.id;
+
+    cubeContainer.appendChild(cubeElement);
+  });
+}
+
+function pushCube() {
+  var cube = generateCube();
+  cubes.push(cube);
+  renderCubes();
+}
+
+function popCube() {
+  if (cubes.length > 0) {
+    cubes.pop();
+    renderCubes();
+  }
+}
+
+function unshiftCube() {
+  var cube = generateCube();
+  cubes.unshift(cube);
+  renderCubes();
+}
+
+function shiftCube() {
+  if (cubes.length > 0) {
+    cubes.shift();
+    renderCubes();
+  }
+}
+
+function deleteCube() {
+  if (cubes.length > 0) {
+    var randomIndex = Math.floor(Math.random() * cubes.length);
+    cubes.splice(randomIndex, 1);
+    renderCubes();
+  }
+}
+
+function sortCubes() {
+  cubes.sort(function (a, b) {
+    return a.id - b.id;
+  });
+  renderCubes();
+}
+
+document.getElementById("push-cube").addEventListener("click", pushCube);
+document.getElementById("pop-cube").addEventListener("click", popCube);
+document.getElementById("unshift-cube").addEventListener("click", unshiftCube);
+document.getElementById("shift-cube").addEventListener("click", shiftCube);
+document.getElementById("delete-cube").addEventListener("click", deleteCube);
+document.getElementById("sort-cubes").addEventListener("click", sortCubes);
+
+// Randomly generate a number of cubes
+var numOfCubes = Math.floor(Math.random() * 8) + 3;
+for (var i = 0; i < numOfCubes; i++) {
+  cubes.push(generateCube());
+}
+
+// Initial rendering of cubes
+renderCubes();
