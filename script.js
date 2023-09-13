f8bc92a91de87eed96173ad31b7fe07bb837bcb2
Levani49 <levani.pachuashvili.1@btu.edu.ge>
2023-09-14 00:00:06 +0400

    it's snowing

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-const userDataDiv = document.getElementById("userData");
-const nextButton = document.getElementById("nextButton");
+const canvas = document.getElementById("snowfallCanvas");
+const ctx = canvas.getContext("2d");
 
-let users = [];
-let currentUserIndex = 0;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-async function fetchUsers() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!response.ok) {
-      throw new Error("Failed to fetch user data");
+const snowflakes = [];
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+class Snowflake {
+  constructor() {
+    this.x = randomInRange(0, canvas.width);
+    this.y = randomInRange(0, canvas.height);
+    this.radius = randomInRange(1, 4);
+    this.speedY = randomInRange(1, 3);
+  }
+
+  update() {
+    this.y += this.speedY;
+
+    if (this.y > canvas.height) {
+      this.x = randomInRange(0, canvas.width);
+      this.y = 0;
     }
-    const data = await response.json();
-    users = data;
-    displayUserData();
-  } catch (error) {
-    userDataDiv.textContent = "Error fetching user data.";
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
   }
 }
 
-function displayUserData() {
-  const currentUser = users[currentUserIndex];
-  userDataDiv.innerHTML = `
-    <p><strong>Name:</strong> ${currentUser.name}</p>
-    <p><strong>Username:</strong> ${currentUser.username}</p>
-    <p><strong>Email:</strong> ${currentUser.email}</p>
-  `;
+function createSnowflakes(numSnowflakes) {
+  for (let i = 0; i < numSnowflakes; i++) {
+    snowflakes.push(new Snowflake());
+  }
 }
 
-nextButton.addEventListener("click", () => {
-  currentUserIndex = (currentUserIndex + 1) % users.length;
-  displayUserData();
-});
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-fetchUsers();
+  for (const snowflake of snowflakes) {
+    snowflake.update();
+    snowflake.draw();
+  }
+
+  requestAnimationFrame(animate);
+}
+
+createSnowflakes(200);
+animate();
